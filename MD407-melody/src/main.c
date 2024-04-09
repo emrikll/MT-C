b4de356c8a095c08c6a003a41f1c146354dfda56
emrikll <emrik.lindahl@gmail.com>
2024-04-09 15:17:58 +0200

    Formated melody code

--- MD407-melody/src/main.c
+++ MD407-melody/src/main.c
@@ -1,25 +1,37 @@
-/**
- * RP2040 FreeRTOS Template - App #3
- *
- * @copyright 2023, Tony Smith (@smittytone)
- * @version   1.4.2
- * @licence   MIT
- *
- */
-#include "main.h"
-#include "portmacro.h"
-#include "projdefs.h"
-#include "stm32f4xx.h"
-#include "stm32f4xx_rcc.h"
-#include "stm32f4xx_tim.h"
-#include "timer.h"
 #include <stdint.h>
+// STM32f4
+#include "misc.h"
+#include "system_stm32f4xx.h"
+#include <stm32f4xx_dac.h>
+#include <stm32f4xx_exti.h>
+#include <stm32f4xx_gpio.h>
+#include <stm32f4xx_rcc.h>
+#include <stm32f4xx_rng.h>
+#include <stm32f4xx_syscfg.h>
+#include <stm32f4xx_tim.h>
+// FreeRTOS
+#include <FreeRTOS.h>
+#include "croutine.h"
+#include "portmacro.h"
+#include "queue.h"
+#include "semphr.h"
+#include "task.h"
+#include "timers.h"
+// Misc
+#include "usart.h"
+#include "ftos.h"
+#include "printf.h"
+#include "timer.h"
 
-/*
+/**
  * GLOBALS
  */
 
+#define AVERAGE_USAGE_INTERVAL_MS 5000
+#define BACKGROUND_TASK_SLEEP_US 1
+
 // Tick counters IDLE
+uint64_t xStartMelody, xStartSpawnBackground;
 uint64_t xTimeInPICO, xTimeOutPICO, xDifferencePICO, xTotalPICO;
 
 // Background Task
@@ -37,40 +49,18 @@ StaticTask_t xTaskBufferBackground[CAPACITY];
 /* Buffer that the task being created will use as its stack.  Note this is
 an array of StackType_t variables.  The size of StackType_t is dependent on
 the RTOS port. */
-StackType_t xStackBackground[CAPACITY][ STACK_SIZE_BACKGROUND ];
+StackType_t xStackBackground[CAPACITY][STACK_SIZE_BACKGROUND];
 
 // Melody Task
-/* Dimensions of the buffer that the task being created will use as its stack.
-NOTE:  This is the number of words the stack will hold, not the number of
-bytes.  For example, if each stack item is 32-bits, and this is set to 100,
-then 400 bytes (100 * 32-bits) will be allocated. */
 #define STACK_SIZE_MELODY 150
-
-/* Structure that will hold the TCB of the task being created. */
 StaticTask_t xTaskBufferMelody;
-
-/* Buffer that the task being created will use as its stack.  Note this is
-an array of StackType_t variables.  The size of StackType_t is dependent on
-the RTOS port. */
 StackType_t xStackMelody[STACK_SIZE_MELODY];
-
 TaskHandle_t handle_melody = NULL;
 
 // Background Task
-/* Dimensions of the buffer that the task being created will use as its stack.
-NOTE:  This is the number of words the stack will hold, not the number of
-bytes.  For example, if each stack item is 32-bits, and this is set to 100,
-then 400 bytes (100 * 32-bits) will be allocated. */
 #define STACK_SIZE_SPAWN_BACKGROUND 150
-
-/* Structure that will hold the TCB of the task being created. */
 StaticTask_t xTaskBufferSpawnBackground;
-
-/* Buffer that the task being created will use as its stack.  Note this is
-an array of StackType_t variables.  The size of StackType_t is dependent on
-the RTOS port. */
 StackType_t xStackSpawnBackground[STACK_SIZE_MELODY];
-
 TaskHandle_t handle_spawn_background = NULL;
 
 /*
@@ -82,12 +72,6 @@ TaskHandle_t handle_spawn_background = NULL;
 // Custom vector to get IRQ working
 #define SCB_VTOR_CUSTOM ((volatile unsigned long *)0xE000ED08)
 
-// Semaphores
-SemaphoreHandle_t semaphore_irq = NULL;
-StaticSemaphore_t xSemaphoreBuffer;
-StaticSemaphore_t xMutexBuffer;
-SemaphoreHandle_t mutex_background_capacity = NULL;
-
 /*
  * HARDWARE INTERRUPT
  */
@@ -95,20 +79,20 @@ SemaphoreHandle_t mutex_background_capacity = NULL;
 TIM_TimeBaseInitTypeDef TIM5_TimeBaseStructure;
 
 void enable_timer_tim5(void) {
-    RCC_ClocksTypeDef RCC_ClocksStatus;
-    RCC_GetClocksFreq(&RCC_ClocksStatus);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
-    TIM5_TimeBaseStructure.TIM_Prescaler = 
-        (uint16_t) (2 * RCC_ClocksStatus.PCLK1_Frequency / 1000000) - 1; // To Hz
-    TIM5_TimeBaseStructure.TIM_Period = 0x000007E8; // To Hz
-    TIM5_TimeBaseStructure.TIM_ClockDivision = 0;
-    TIM5_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_TimeBaseInit(TIM5, &TIM5_TimeBaseStructure);
-    /* TIM Interrupts enable */
-    TIM_ITConfig(TIM5, TIM_IT_Update, ENABLE);
-    TIM_SetAutoreload(TIM5, 0x000007E8);
-    /* TIM5 enable counter */
-    TIM_Cmd(TIM5, ENABLE);
+  RCC_ClocksTypeDef RCC_ClocksStatus;
+  RCC_GetClocksFreq(&RCC_ClocksStatus);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+  TIM5_TimeBaseStructure.TIM_Prescaler =
+      (uint16_t)(2 * RCC_ClocksStatus.PCLK1_Frequency / 1000000) - 1; // To Hz
+  TIM5_TimeBaseStructure.TIM_Period = 0x000007E8;                     // To Hz
+  TIM5_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM5_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM5, &TIM5_TimeBaseStructure);
+  /* TIM Interrupts enable */
+  TIM_ITConfig(TIM5, TIM_IT_Update, ENABLE);
+  TIM_SetAutoreload(TIM5, 0x000007E8);
+  /* TIM5 enable counter */
+  TIM_Cmd(TIM5, ENABLE);
 }
 
 void EnableTimerInterrupt() {
@@ -128,15 +112,9 @@ void TIM5_IRQHandler() {
     TIM_ClearITPendingBit(TIM5, TIM_IT_Update);
 
     BaseType_t higher_priority_task_woken = pdFALSE;
-    /* Make sure that interrupt flag is set */
-    /* ISR FUNCTION BODY USING A SEMAPHORE */
-
-    // xStartISR = time_us_64();
 
     vTaskNotifyGiveFromISR(handle_melody, &higher_priority_task_woken);
 
-    // Signal the alert clearance task
-    // xSemaphoreGiveFromISR(semaphore_irq, &higher_priority_task_woken);
     // Exit to context switch if necessary
     portYIELD_FROM_ISR(higher_priority_task_woken);
   }
@@ -147,10 +125,9 @@ void TIM5_IRQHandler() {
  */
 
 void task_melody(void *vParameters) {
-  printf_("Begin task\n\r");
   int volume = 10;
   int bin = 1;
-
+  xStartMelody = time_us();
   while (1) {
     ulTaskNotifyTake(pdFALSE, portMAX_DELAY);
     if (bin) {
@@ -164,31 +141,34 @@ void task_melody(void *vParameters) {
 }
 
 void task_background(void *vParameters) {
-    uint32_t endtime = 0;
-    const TickType_t xDelay = 1 / portTICK_PERIOD_MS;
+  uint32_t endtime = 0;
+  const TickType_t xDelay = 1 / portTICK_PERIOD_MS;
 
-    while (1) {
-        endtime = time_us() + BACKGROUND_TASK_SLEEP_US;
-        while(time_us() < endtime){}
-        vTaskDelay(xDelay);
+  while (1) {
+    endtime = time_us() + BACKGROUND_TASK_SLEEP_US;
+    while (time_us() < endtime) {
     }
+    vTaskDelay(xDelay);
+  }
 }
 
 void task_spawn_background(void *vParameters) {
-    const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
-    TickType_t xLastWakeTime;
-    xLastWakeTime = xTaskGetTickCount();
-
-  printf_("Begin task spawn\n\r");
-    while (1) {
-        if (capacity_background_task < CAPACITY) {
-            xTaskCreateStatic(task_background, "BACKGROUND", 64, NULL, 1, xStackBackground[capacity_background_task], &xTaskBufferBackground[capacity_background_task]);
-            capacity_background_task++;
-            vTaskDelayUntil(&xLastWakeTime,xDelay);
-        }else {
-            vTaskSuspend(NULL);
-        }
+  const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime;
+  xLastWakeTime = xTaskGetTickCount();
+  xStartSpawnBackground = time_us();
+  while (1) {
+    if (capacity_background_task < CAPACITY) {
+      xTaskCreateStatic(task_background, "BACKGROUND", 64, NULL, 1,
+                        xStackBackground[capacity_background_task],
+                        &xTaskBufferBackground[capacity_background_task]);
+      capacity_background_task++;
+      vTaskDelayUntil(&xLastWakeTime, xDelay);
+    } else {
+      printf_("SPAWN %u, MELODY %u\n\r", xStartSpawnBackground, xStartMelody);
+      vTaskSuspend(NULL);
     }
+  }
 }
 
 /*
@@ -202,26 +182,19 @@ int main() {
   printf_("Hardware begin\n\r");
   NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
   EnableTimerInterrupt();
-
-  semaphore_irq = xSemaphoreCreateBinaryStatic(&xSemaphoreBuffer);
-  configASSERT(semaphore_irq != NULL);
-
   enable_timer();
   enable_timer_tim5();
   printf_("Hardware done\n\r");
 
-  handle_melody = xTaskCreateStatic(task_melody, "MELODY", 128, NULL, 1, xStackMelody, &xTaskBufferMelody);
+  // Task handlers
+  handle_melody = xTaskCreateStatic(task_melody, "MELODY", 128, NULL, 1,
+                                    xStackMelody, &xTaskBufferMelody);
+  handle_spawn_background =
+      xTaskCreateStatic(task_spawn_background, "SPAWN BACKGROUND", 128, NULL, 1,
+                        xStackSpawnBackground, &xTaskBufferSpawnBackground);
 
-  handle_spawn_background = xTaskCreateStatic(task_spawn_background, "SPAWN BACKGROUND", 128, NULL, 1, xStackSpawnBackground, &xTaskBufferSpawnBackground); 
-  // Start the FreeRTOS scheduler if any of the tasks are good
-  // start_time = time_us();
   // Start the scheduler
   vTaskStartScheduler();
-
-  // We should never get here, but just in case...
-  while (1) {
-    // NOP
-  };
 }
 
 /*
@@ -270,7 +243,6 @@ void task_cpu_usage(TimerHandle_t timer) {
   float usage = ((float)average_time - (float)xTotalPICO) / (float)average_time;
 
   xTotalPICO = 0;
-
 }
 
 /*
