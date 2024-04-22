dcc2b2038dfcde8d54fdc7444621ffae3067efb5
emrikll <emrik.lindahl@gmail.com>
2024-04-22 09:15:18 +0200

    A version of fft

--- MD407-concurrency/src/main.c
+++ MD407-concurrency/src/main.c
@@ -1,18 +1,16 @@
-
-
+#include <FreeRTOS.h>
 #include <stdint.h>
 #include <stm32f4xx_gpio.h>
 #include <stm32f4xx_rcc.h>
-#include <FreeRTOS.h>
 #include "FreeRTOSConfig.h"
 #include "portmacro.h"
+#include "printf.h"
 #include "projdefs.h"
+#include "semphr.h"
 #include "task.h"
 #include "timer.h"
 #include "timers.h"
-#include "semphr.h"
 #include "usart.h"
-#include "printf.h"
 
 #define CAPACITY_HIGH 5
 #define CAPACITY_MEDIUM 3
@@ -32,13 +30,13 @@ then 400 bytes (100 * 32-bits) will be allocated.  6817583
 
 /* Structure that will hold the TCB of the tasks being created. */
 StaticTask_t xTaskBufferLow[CAPACITY_LOW];
-StackType_t xStackLow[CAPACITY_LOW][ STACK_SIZE ];
+StackType_t xStackLow[CAPACITY_LOW][STACK_SIZE];
 
 StaticTask_t xTaskBufferMedium[CAPACITY_MEDIUM];
-StackType_t xStackMedium[CAPACITY_MEDIUM][ STACK_SIZE ];
+StackType_t xStackMedium[CAPACITY_MEDIUM][STACK_SIZE];
 
 StaticTask_t xTaskBufferHigh[CAPACITY_HIGH];
-StackType_t xStackHigh[CAPACITY_HIGH][ STACK_SIZE ];
+StackType_t xStackHigh[CAPACITY_HIGH][STACK_SIZE];
 
 uint32_t start_time = 0;
 
@@ -48,147 +46,118 @@ uint8_t done = 0;
 StaticSemaphore_t xMutexBuffer;
 static SemaphoreHandle_t shared_variable_lock;
 /*
- ** Tasks 
-*/
+ ** Tasks
+ */
 
 void increment_shared() {
-    if (done) {
-        vTaskSuspend(NULL);
-    }
-    if (xSemaphoreTake(shared_variable_lock, portMAX_DELAY) != pdTRUE) {
-        printf_("Something went wrong\r\n");
-    }
-    shared_variable++;
-    if(shared_variable == MAX_VALUE) {
-        done = 1;
-        uint32_t end_time = time_us();
+  if (done) {
+    vTaskSuspend(NULL);
+  }
+  if (xSemaphoreTake(shared_variable_lock, portMAX_DELAY) != pdTRUE) {
+    printf_("Something went wrong\r\n");
+  }
+  shared_variable++;
+  if (shared_variable == MAX_VALUE) {
+    done = 1;
+    uint32_t end_time = time_us();
 
-        printf_("Reached max value at after: %d\r\n", end_time - start_time);
-    } 
-    
-    xSemaphoreGive(shared_variable_lock);
+    printf_("Reached max value at after: %d\r\n", end_time - start_time);
+  }
+
+  xSemaphoreGive(shared_variable_lock);
 }
 
 void low_priority_task(void *parameter) {
-    while (1) {
-        increment_shared();
-        vTaskDelay(FREQUENCY_LOW);
-    }
+  while (1) {
+    increment_shared();
+    vTaskDelay(FREQUENCY_LOW);
+  }
 }
 
 void medium_priority_task(void *parameter) {
-    while (1) {
-        increment_shared();
-        vTaskDelay(FREQUENCY_MEDIUM);
-    }
+  while (1) {
+    increment_shared();
+    vTaskDelay(FREQUENCY_MEDIUM);
+  }
 }
 
 void high_priority_task(void *parameter) {
-    while (1) {
-        increment_shared();
-        vTaskDelay(FREQUENCY_HIGH);
-    }
+  while (1) {
+    increment_shared();
+    vTaskDelay(FREQUENCY_HIGH);
+  }
 }
 
-int main(void)
-{
-    enable_timer();
-    enable_usart();
-    printf_("\n\rInit\n\r");
+int main(void) {
+  enable_timer();
+  enable_usart();
+  printf_("\n\rInit\n\r");
 
-    shared_variable_lock = xSemaphoreCreateMutexStatic( &xMutexBuffer );
-    configASSERT(shared_variable_lock != NULL);
+  shared_variable_lock = xSemaphoreCreateMutexStatic(&xMutexBuffer);
+  configASSERT(shared_variable_lock != NULL);
 
-    for (int i = 0; i < CAPACITY_HIGH; i++) {
-        xTaskCreateStatic(
-            high_priority_task, 
-            "High priority", 
-            STACK_SIZE, 
-            NULL, 
-            3, 
-            xStackHigh[i],
-            &xTaskBufferHigh[i]
-        );
-    }
+  for (int i = 0; i < CAPACITY_HIGH; i++) {
+    xTaskCreateStatic(high_priority_task, "High priority", STACK_SIZE, NULL, 3,
+                      xStackHigh[i], &xTaskBufferHigh[i]);
+  }
 
-    for (int i = 0; i < CAPACITY_MEDIUM; i++) {
-        xTaskCreateStatic(
-            medium_priority_task, 
-            "Medium priority", 
-            STACK_SIZE, 
-            NULL, 
-            2, 
-            xStackMedium[i],
-            &xTaskBufferMedium[i]
-        );
-    }
+  for (int i = 0; i < CAPACITY_MEDIUM; i++) {
+    xTaskCreateStatic(medium_priority_task, "Medium priority", STACK_SIZE, NULL,
+                      2, xStackMedium[i], &xTaskBufferMedium[i]);
+  }
 
-    for (int i = 0; i < CAPACITY_LOW; i++) {
-        xTaskCreateStatic(
-            low_priority_task, 
-            "Low priority", 
-            STACK_SIZE, 
-            NULL, 
-            1, 
-            xStackLow[i],
-            &xTaskBufferLow[i]
-        );
-    }
+  for (int i = 0; i < CAPACITY_LOW; i++) {
+    xTaskCreateStatic(low_priority_task, "Low priority", STACK_SIZE, NULL, 1,
+                      xStackLow[i], &xTaskBufferLow[i]);
+  }
 
-
-    printf_("Starting scheduler...\n\r");
-    start_time = time_us();
-    vTaskStartScheduler();
-
+  printf_("Starting scheduler...\n\r");
+  start_time = time_us();
+  vTaskStartScheduler();
 }
 
-void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
-                                    StackType_t **ppxIdleTaskStackBuffer,
-                                    uint32_t *pulIdleTaskStackSize )
-{
-	static StaticTask_t xIdleTaskTCB;
-	static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize) {
+  static StaticTask_t xIdleTaskTCB;
+  static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
 
-    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
-    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
-    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+  *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
 }
 
-void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
-                                     StackType_t ** ppxTimerTaskStackBuffer,
-                                     uint32_t * pulTimerTaskStackSize )
-{
-    /* If the buffers to be provided to the Timer task are declared inside this
-     * function then they must be declared static - otherwise they will be allocated on
-     * the stack and so not exists after this function exits. */
-    static StaticTask_t xTimerTaskTCB;
-    static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
- 
-    /* Pass out a pointer to the StaticTask_t structure in which the Idle
-     * task's state will be stored. */
-    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
- 
-    /* Pass out the array that will be used as the Timer task's stack. */
-    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
- 
-    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
-     * Note that, as the array is necessarily of type StackType_t,
-     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
+                                    StackType_t **ppxTimerTaskStackBuffer,
+                                    uint32_t *pulTimerTaskStackSize) {
+  /* If the buffers to be provided to the Timer task are declared inside this
+   * function then they must be declared static - otherwise they will be
+   * allocated on the stack and so not exists after this function exits. */
+  static StaticTask_t xTimerTaskTCB;
+  static StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
+
+  /* Pass out a pointer to the StaticTask_t structure in which the Idle
+   * task's state will be stored. */
+  *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+
+  /* Pass out the array that will be used as the Timer task's stack. */
+  *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+
+  /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+   * Note that, as the array is necessarily of type StackType_t,
+   * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+  *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
 }
 
 /*
-* PERFORMANCE FUNCTIONS 
-*/
+ * PERFORMANCE FUNCTIONS
+ */
 /**
  * @brief Handler for when tasks switch in.
  */
-void handle_switched_in(int* pxCurrentTCB) {
-}
+void handle_switched_in(int *pxCurrentTCB) {}
 
 /**
  * @brief Handler for when tasks switch out.
  */
-void handle_switched_out(int* pxCurrentTCB) {
-}
-
+void handle_switched_out(int *pxCurrentTCB) {}
---
+++ fft-script/src/main.rs
@@ -0,0 +1,154 @@
+#![feature(iter_next_chunk)]
+
+use std::{env, error::Error, fmt::Write, io::{BufRead, BufReader}, process::exit};
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
+use plotters::prelude::*;
+
+use realfft::RealFftPlanner;
+use rustfft::num_complex::Complex;
+use realfft::num_traits::Pow;
+
+use std::str;
+const UPPER_LIMIT: f64 = 0.250;
+const LOWER_LIMIT: f64 = 0.220;
+const INTERVAL: f64 = 0.00000001;
+const HERTZ: f64 = 1.0/INTERVAL;
+const LINES: usize = 10_000;
+const GRAPH_NAME: &str = "graph.png";
+
+fn add_iterator_to_csv_result<T>(pb: &ProgressBar, result_time_vec: &mut Vec<f64>, result_value_vec: &mut Vec<f64>, iter: &[Result<String, T>]){
+
+        for line in iter {
+        pb.inc(1);
+            if let Ok(string) = line {
+                let split_line: Vec<&str> = string.split(',').collect();
+                result_value_vec.push(split_line[1].parse().unwrap());
+                result_time_vec.push(split_line[0].parse().unwrap());
+            }
+        }
+        
+
+}
+
+
+fn parse_csv(filename: &String) -> Result<(Vec<f64>,Vec<f64>), Box<dyn Error>> {
+    let mut file = std::fs::File::open(filename.clone()).unwrap();
+    println!("Indexing file...");
+    let mut buf = BufReader::new(&file);
+    let count = buf.lines().count();
+    
+    file = std::fs::File::open(filename).unwrap();
+
+    buf = BufReader::new(&file);
+
+    let mut lines = buf.lines();
+    
+    let pb = ProgressBar::new(count as u64);
+    pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta})")
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+        .progress_chars("#>-"));
+    lines.next();
+    let mut result_value_vec: Vec<f64> = vec![];
+    let mut result_time_vec: Vec<f64> = vec![];
+
+    loop {
+        let chunk = lines.next_chunk::<LINES>();
+        if let Err(last) = chunk {
+            add_iterator_to_csv_result(&pb, &mut result_time_vec, &mut result_value_vec, last.as_slice());
+            break
+        }
+
+        let ok_chunk = chunk.unwrap();
+        let slice_chunk = ok_chunk.as_slice();
+        add_iterator_to_csv_result(&pb, &mut result_time_vec, &mut result_value_vec, slice_chunk);        
+
+    }
+         
+    Ok((result_value_vec, result_time_vec))
+}
+
+fn make_fft(mut value_vec: Vec<f64>) -> Vec<Complex<f64>> {
+    // make a planner
+    let mut real_planner = RealFftPlanner::<f64>::new();
+    // create a FFT
+    let r2c = real_planner.plan_fft_forward(value_vec.len());
+    // make a dummy real-valued signal (filled with zeros)
+    let mut indata = r2c.make_input_vec();
+    // make a vector for storing the spectrum
+    let mut spectrum = r2c.make_output_vec();
+
+    // forward transform the signal
+    let mut test = value_vec.as_mut_slice();
+    r2c.process(&mut test, &mut spectrum).unwrap();
+
+    return spectrum
+
+}
+
+
+fn main() {
+        let (mut a, mut b) = parse_csv(&"/home/emrik/Thesis/repos/MT-C/fft-script/analog.csv".to_string()).unwrap();
+    //println!("{:?}, {:?}",a,b);
+    
+    let mut spectrum = make_fft(a);
+
+    //println!("{:?}", spectrum[0].re);
+
+    let mut real = Vec::new();
+    let mut img = Vec::new();
+    let mut format: Vec<(f32, f32)> = vec![];
+    let mut low_x = 0 as f64;
+    let mut low_y = 0 as f64;
+    let mut high_x = 0 as f64;
+    let mut high_y = 0 as f64;
+    let freq_resolution = HERTZ/(spectrum.len() as f64);
+    
+    for (index,complex) in spectrum.into_iter().enumerate() {
+        real.push(complex.re);
+        img.push(complex.im);
+        //format.push((complex.re as f32,complex.im as f32));
+        
+        let mut mag = (complex.re.pow(2) as f64 + complex.im.pow(2) as f64).sqrt();
+        let mut freq = freq_resolution * index as f64; 
+
+        if freq < low_x{
+            low_x = freq;
+        }
+        if freq > high_x {
+            high_x = freq;
+        }
+        if mag < low_y{
+            low_y = mag;
+        }
+        if mag > high_y {
+            high_y = mag;
+        }
+        
+        format.push((freq as f32, mag as f32));
+    }
+
+    //println!("REAL {:?}", real);
+    //println!("IMG {:?}", img);
+    //println!("Format {:?}", format);
+
+    let root = BitMapBackend::new(GRAPH_NAME, (1920, 1080)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Frequency as a FFT", ("sans-serif", 30).into_font())
+        .margin(5)
+        .x_label_area_size(50)
+        .y_label_area_size(50)
+        .build_cartesian_2d(low_x as f32..1000000 as f64 as f32, low_y as f32..500000 as f32).unwrap();
+
+    chart.configure_mesh().draw().unwrap();
+
+    chart
+        .draw_series(LineSeries::new(
+            format,
+            &RED,
+        )).unwrap();
+
+    println!("Done! Saved as {}", GRAPH_NAME);
+    root.present().unwrap();
+}
